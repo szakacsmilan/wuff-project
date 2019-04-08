d0e02bd16c683c0942fe13ac3ba958a38b4a0089
milanszakacs <milan.szakacs@gmail.com>
2019-04-08 13:45:00 +0200

    Initial commit

---
+++ src/main/java/com/codecool/wuff/wuffproject/WuffProjectApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.wuff.wuffproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WuffProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WuffProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codecool/wuff/wuffproject/WuffProjectApplicationTests.java
@@ -0,0 +1,16 @@
+package com.codecool.wuff.wuffproject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WuffProjectApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}

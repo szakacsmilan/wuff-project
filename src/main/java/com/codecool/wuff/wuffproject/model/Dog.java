2adc83d1f9f976d5ef5360473a242b549da83b96
Miklós Balázs <bazsi2382@gmail.com>
2019-05-06 10:35:47 +0200

    dog class

---
+++ src/main/java/com/codecool/wuff/wuffproject/model/Dog.java
@@ -0,0 +1,39 @@
+package com.codecool.wuff.wuffproject.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Transient;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class Dog {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+    private String breed;
+    private String keeperName;
+    private LocalDate birthDate;
+
+    @Transient
+    private long age;
+
+    public void calculateAge(){
+        if(birthDate != null){
+            age = ChronoUnit.YEARS.between(birthDate, LocalDate.now());
+        }
+    }
+}

42ffdc7469fa64866a7e061efdf4a654332f7256
milanszakacs <milan.szakacs@gmail.com>
2019-05-08 16:38:19 +0200

    registration email text + password confirmation fixed

--- src/main/resources/static/javascript/index.js
+++ src/main/resources/static/javascript/index.js
@@ -1,11 +1,14 @@
-
 var password = document.getElementById('password1');
 var confirmPassword = document.getElementById('password2');
+var button = document.getElementById('submitbutt');
 
+button.addEventListener("click", check);
 
 function check() {
     if (password.value === confirmPassword.value)  {
         document.getElementById('submitbutt').click();
+        confirmPassword.setCustomValidity("");
+
     } else {
         confirmPassword.setCustomValidity("Passwords Don't Match");
     }

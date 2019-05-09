32f5e8e7205a000b2ccd79f955f74ced433828c4
milanszakacs <milan.szakacs@gmail.com>
2019-05-09 21:38:11 +0200

    spots css modified

---
+++ src/main/java/com/codecool/wuff/wuffproject/model/Mail.java
@@ -0,0 +1,17 @@
+package com.codecool.wuff.wuffproject.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Mail {
+
+    private String from;
+    private String to;
+    private String subject;
+    private String content;
+
+}
